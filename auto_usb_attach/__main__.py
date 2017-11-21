9777f62a27aba5bc351f5760f8808e0417b850f3
Stephen Czetty <stephen@houseofczetty.com>
2017-11-21 15:15:05 -0600

    Change DeviceMonitor to use pyudev.MonitorObserver
    This creates a thread to handle the events.

--- auto_usb_attach/__main__.py
+++ auto_usb_attach/__main__.py
@@ -3,6 +3,7 @@
 import sys
 from functools import partial
 from typing import List, Dict
+from threading import Lock
 
 from .options import Options
 from .xendomain import XenDomain, XenError
@@ -12,15 +13,17 @@ from .xenusb import XenUsb
 
 opts = None
 device_map = {}
+device_map_lock = Lock()
 
 
-def add_device(domain: XenDomain, root_devices: List[Device], device: Device):
-    if device.is_a_device_we_care_about(root_devices) and device.sys_name not in device_map:
+def add_device(domain: XenDomain, device: Device):
+    if device.sys_name not in device_map:
         opts.print_verbose("Device added: {0}".format(device))
 
         try:
             dev_map = domain.attach_device_to_xen(device)
-            device_map[device.sys_name] = dev_map
+            with device_map_lock:
+                device_map[device.sys_name] = dev_map
         except XenError:
             pass
 
@@ -29,19 +32,8 @@ def remove_device(domain: XenDomain, device: Device):
     if device.sys_name in device_map:
         opts.print_verbose("Removing device: {0}".format(device))
         if domain.detach_device_from_xen(device_map[device.sys_name]):
-            del device_map[device.sys_name]
-
-
-def get_connected_devices(domain: XenDomain, root_devices: List[Device]) -> Dict[str, XenUsb]:
-    for monitored_device in root_devices:
-        for device in monitored_device.devices_of_interest():
-            opts.print_verbose("Found at startup: {0.device_path}".format(device))
-            dev_map = domain.find_device_mapping(device.sys_name)
-            if dev_map is None:
-                dev_map = domain.attach_device_to_xen(device)
-            if dev_map is not None:
-                device_map[device.sys_name] = dev_map
-    return device_map
+            with device_map_lock:
+                del device_map[device.sys_name]
 
 
 def remove_disconnected_devices(domain: XenDomain, devices: Dict[str, XenUsb]):
@@ -56,14 +48,18 @@ def main(args: List[str]) -> None:
 
     with XenDomain(opts) as xen_domain:
         monitor = DeviceMonitor(opts, xen_domain)
-        root_devices = [monitor.add_monitored_device(x) for x in opts.hubs]
-        monitor.device_added += partial(add_device, xen_domain, root_devices)
+        for h in opts.hubs:
+            monitor.add_monitored_device(h)
+
+        monitor.device_added += partial(add_device, xen_domain)
         monitor.device_removed += partial(remove_device, xen_domain)
 
         try:
-            device_map = get_connected_devices(xen_domain, root_devices)
-            remove_disconnected_devices(xen_domain, device_map)
+            with device_map_lock:
+                device_map = monitor.get_connected_devices(xen_domain)
+                remove_disconnected_devices(xen_domain, device_map)
             monitor.monitor_devices()
+            monitor.wait()
         except KeyboardInterrupt:
             pass
 
--- auto_usb_attach/devicemonitor.py
+++ auto_usb_attach/devicemonitor.py
@@ -1,6 +1,6 @@
-from typing import Dict, Tuple, Iterable, Optional, cast
+from typing import Dict
 
-from auto_usb_attach.xenusb import XenUsb
+from .xenusb import XenUsb
 from .device import Device
 from .options import Options
 from .xendomain import XenDomain
@@ -23,31 +23,62 @@ class DeviceMonitor:
         if not dev.is_a_hub():
             raise RuntimeError("Device {0} is not a hub".format(dev.sys_name))
 
+        if dev not in self.__root_devices:
+            self.__root_devices.append(dev)
+
         return dev
+
+    def get_connected_devices(self, domain: XenDomain) -> Dict[str, XenUsb]:
+        device_map = {}
+        for monitored_device in self.__root_devices:
+            for device in monitored_device.devices_of_interest():
+                self.__options.print_verbose("Found at startup: {0.device_path}".format(device))
+                dev_map = domain.find_device_mapping(device.sys_name)
+                if dev_map is None:
+                    dev_map = domain.attach_device_to_xen(device)
+                if dev_map is not None:
+                    device_map[device.sys_name] = dev_map
+        return device_map
 
     # This method never returns unless there's an exception.  Good?  Bad?
     def monitor_devices(self) -> None:
         monitor = pyudev.Monitor.from_netlink(self.__context)
         monitor.filter_by('usb')
 
-        for device in cast(Iterable[Optional[pyudev.Device]], iter(monitor.poll, None)):
-            if device is None:
-                return
-
+        def monitor_callback(device: pyudev.Device) -> None:
             device = Device(device)
             self.__options.print_very_verbose('{0.action} on {0.device_path}'.format(device))
             if device.action == "add":
-                self.device_added(device)
+                if device.is_a_device_we_care_about(self.__root_devices):
+                    self.device_added(device)
             elif device.action == "remove":
                 self.device_removed(device)
+
+        self.__observer = pyudev.MonitorObserver(monitor, callback=monitor_callback)
+        self.__observer.start()
+
+    def wait(self):
+        if self.__observer is not None:
+            self.__observer.join()
 
     def __init__(self, opts: Options, xen_domain: XenDomain):
         self.__context = pyudev.Context()
         self.__options = opts
         self.__domain = xen_domain
+        self.__root_devices = []
+        self.__observer = None
 
         self.device_added = Event()
         self.device_removed = Event()
 
     def __repr__(self):
         return "DeviceMonitor({!r}, {!r})".format(self.__options, self.__domain)
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        if self.__observer is not None:
+            self.__observer.stop()
+            self.__started = False
+            self.__observer = None
