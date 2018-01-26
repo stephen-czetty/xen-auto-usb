d69bcb790eee15c520523a8ac9ae913eb670ae5f
Stephen Czetty <stephen@houseofczetty.com>
2018-01-26 13:14:26 -0600

    Working wrapper, but execve drops perms, so we'll need to set SUDO_UID.

--- wrapper.c
+++ wrapper.c
@@ -6,19 +6,27 @@
 
 int main(int argc, char *argv[]) {
     const char *scriptName = "auto-usb-attach.py";
+    const char *environmentVariable = "WRAPPER";
     char scriptPath[PATH_MAX];
+    char wrapperPath[PATH_MAX];
 
     /* /proc/self/exe is a symlink to this executable */
-    readlink("/proc/self/exe", scriptPath, PATH_MAX);
+    readlink("/proc/self/exe", wrapperPath, PATH_MAX);
+    strcpy(scriptPath, wrapperPath);
     char *lastSlashPos = strrchr(scriptPath, '/');
 
     if (lastSlashPos - scriptPath + strlen(scriptName) > PATH_MAX-1)
+    {
+	printf("%s\n", scriptPath);
+	printf("Path too long: %ld\n", lastSlashPos - scriptPath + strlen(scriptName));
         return 0;
+    }
 
     strcpy(lastSlashPos+1, scriptName);
     printf("%s", scriptPath);
 
-
-    char *emptyEnviron[] = { NULL };
-    return execve(scriptName, argv, emptyEnviron);
+    char env[strlen(wrapperPath) + strlen(environmentVariable) + 2];
+    sprintf(env, "%s=%s", environmentVariable, wrapperPath);
+    char *environ[] = { env, NULL };
+    return execve(scriptName, argv, environ);
 }
