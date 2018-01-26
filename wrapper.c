66099c662e907fe6a4d51e53b4e6709ae5ccbaa8
Stephen Czetty <stephen@houseofczetty.com>
2018-01-26 12:36:24 -0600

    Initial, non-functional c-wrapper.

---
+++ wrapper.c
@@ -0,0 +1,24 @@
+#include <unistd.h>
+#include <string.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <linux/limits.h>
+
+int main(int argc, char *argv[]) {
+    const char *scriptName = "auto-usb-attach.py";
+    char scriptPath[PATH_MAX];
+
+    /* /proc/self/exe is a symlink to this executable */
+    readlink("/proc/self/exe", scriptPath, PATH_MAX);
+    char *lastSlashPos = strrchr(scriptPath, "/");
+
+    if (lastSlashPos - scriptPath + strlen(scriptName) > PATH_MAX-1)
+        return 0
+
+    strcpy(lastSlashPos+1, scriptName);
+    printf("%s", scriptPath);
+
+
+    /*return execv("/path/to/script.py", argv);*/
+    return 0
+}
